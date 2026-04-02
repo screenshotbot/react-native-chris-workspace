6bdab3fb4fe7562025b6745ca57f0ab3c8d27c1c
Arnold Noronha <arnold@screenshotbot.io>
2026-04-02 16:16:17 -0400

    ..

--- android/app/src/androidTest/java/com/testapp/IsolatedTest.kt
+++ android/app/src/androidTest/java/com/testapp/IsolatedTest.kt
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith
 import com.facebook.testing.screenshot.ViewHelpers
 import com.facebook.testing.screenshot.Screenshot
 import org.junit.Assert.*;
+import com.facebook.react.interfaces.*
 
 @RunWith(AndroidJUnit4::class)
 class IsolatedTest {
@@ -24,7 +25,12 @@ class IsolatedTest {
         val app = context.applicationContext as MainApplication
         val surface = app.reactHost.createSurface(context, "SimpleTestComponent", null)
         assertEquals("SimpleTestComponent", surface.moduleName)
-        surface.start()
+
+        // TODO: we aren't 100% sure if prerender() and start() are being called the way we want it to.
+        // We probably want to create a ReactHost directly instead of taking it from the MainApplication... probably
+        // Also look up bridge-less mode.
+        assertGoodTask(surface.prerender())
+
 
         assertNotNull(surface.view)
 
@@ -33,9 +39,17 @@ class IsolatedTest {
             .setExactWidthPx(1000)
             .layout()
 
-        surface.prerender()
+
+        val ti = surface.start()
+        assertGoodTask(ti)
 
         Screenshot.snap(surface.view!!)
             .record()
     }
+}
+
+fun assertGoodTask(ti : TaskInterface<Void>) {
+    ti.waitForCompletion()
+    assertFalse(ti.isFaulted())
+    assertTrue(ti.isCompleted())
 }
